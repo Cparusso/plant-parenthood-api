bd3b85a1ba3b1b0461303cc929f8393ccef57e0e
Charlie Russo <cparusso@gmail.com>
2019-11-10 23:11:54 -0500

    Add duplicate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ alicia = User.create(
 
 puts "Growing Plants"
 pilea = Plant.create(
-    picture: "placeholder",
+    picture: "https://images-na.ssl-images-amazon.com/images/I/71Mu-27B53L._SX425_.jpg",
     binomial_name: "Pilea Peperomioides",
     common_name: "Coin Plant / Chinese Money Plant",
     water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
@@ -43,7 +43,7 @@ pilea = Plant.create(
     bio: "Pilea peperomioides (Urticaceae) is an Asiatic perennial herb that is native to Yunnan Province in Southern China at the foot of the Himalayas. For a long time, this plant has been a mystery. That is because this plant, like many other Pilea, is insignificant in economic value (weedy), and like other Pilea, not much attention was paid to it. It was found to have been brought to Norway by a Norwegian missionary in 1946, upon the expulsion of religion and foreigners by Chairman Mao. From Norway, it was distributed by friends to friends, and eventually all over the world as a houseplant."
 )
 zz = Plant.create(
-    picture: "placeholder",
+    picture: "https://cdn.shopify.com/s/files/1/1706/1307/products/Zamioculcas-zamiifolia-ZZ-Plant.jpg?v=1535278807",
     binomial_name: "Zamioculcas Zamiifolia",
     common_name: "ZZ Plant",
     water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
@@ -54,7 +54,73 @@ zz = Plant.create(
     bio: "Zamioculcas zamiifolia, affectionately called the ZZ plant or Zanzibar Gem, is a tropical plant in the aroid family, Araceae. It is native to Zanzibar, Kenya, and Eastern Africa. You might spot large potato-like rhizomes under the surface of its potting mix. Having evolved in drier conditions than most other aroids, these plants have evolved rhizomes that store water to help the ZZ plant survive drought in its natural environment. The rhizomes are actually underground stems, and what you see above ground are the stems' leaves. Using these underground rhizomes, the plant slowly creeps over time to new locations, spawning new aboveground leaves along the way."
 )
 snake = Plant.create(
-    picture: "placeholder",
+    picture: "https://images-na.ssl-images-amazon.com/images/I/51YIe2wBxlL._SX425_.jpg",
+    binomial_name: "Sansevieria Trifasciata",
+    common_name: "Snake Plant / Mother In-law's Tongue",
+    water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
+    light: "Thrives in medium to bright indirect light, but can tolerate low indirect light.",
+    humidity: "Average humidity levels are recommended, but these plants can tolerate dry air conditions.",
+    succulent: false,
+    pet_friendly: false,
+    bio: "Native to southern and central Africa, the Sansevieria trifasciata 'Laurentii' is a cultivar of snake plant with yellow variegated edges. Snake plants are tropical plants that are also succulents. Often positioned as low light plants, they are actually medium to bright light plants that tolerate, but not thrive in low light. Like other succulent plants, they have a modified form of photosynthesis, where in order to preserve water, the plant opens its pores exclusively at night. However, since photosynthesis only happens with daylight, the plant stores carbon dioxide acquired at night for use during the day, and releases oxygen (one of the end products of photosynthesis) at night when the pores open. This helps it to maximize efficiency in cleaning the air, as found in study conducted by NASA."
+)
+pilea2 = Plant.create(
+    picture: "https://images-na.ssl-images-amazon.com/images/I/71Mu-27B53L._SX425_.jpg",
+    binomial_name: "Pilea Peperomioides",
+    common_name: "Coin Plant / Chinese Money Plant",
+    water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
+    light: "Thrives in bright direct light, but can tolerate bright to medium indirect light.",
+    humidity: "These plants love a high himidity environment.",
+    succulent: false,
+    pet_friendly: true,
+    bio: "Pilea peperomioides (Urticaceae) is an Asiatic perennial herb that is native to Yunnan Province in Southern China at the foot of the Himalayas. For a long time, this plant has been a mystery. That is because this plant, like many other Pilea, is insignificant in economic value (weedy), and like other Pilea, not much attention was paid to it. It was found to have been brought to Norway by a Norwegian missionary in 1946, upon the expulsion of religion and foreigners by Chairman Mao. From Norway, it was distributed by friends to friends, and eventually all over the world as a houseplant."
+)
+zz2 = Plant.create(
+    picture: "https://cdn.shopify.com/s/files/1/1706/1307/products/Zamioculcas-zamiifolia-ZZ-Plant.jpg?v=1535278807",
+    binomial_name: "Zamioculcas Zamiifolia",
+    common_name: "ZZ Plant",
+    water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
+    light: "Thrives in medium to low indirect light. Can tolerate bright indirect light. Not suited for intense, direct sun.",
+    humidity: "Average indoor humidity (around 40% relative humidity) is fine for the zz plant.",
+    succulent: true,
+    pet_friendly: false,
+    bio: "Zamioculcas zamiifolia, affectionately called the ZZ plant or Zanzibar Gem, is a tropical plant in the aroid family, Araceae. It is native to Zanzibar, Kenya, and Eastern Africa. You might spot large potato-like rhizomes under the surface of its potting mix. Having evolved in drier conditions than most other aroids, these plants have evolved rhizomes that store water to help the ZZ plant survive drought in its natural environment. The rhizomes are actually underground stems, and what you see above ground are the stems' leaves. Using these underground rhizomes, the plant slowly creeps over time to new locations, spawning new aboveground leaves along the way."
+)
+snake2 = Plant.create(
+    picture: "https://images-na.ssl-images-amazon.com/images/I/51YIe2wBxlL._SX425_.jpg",
+    binomial_name: "Sansevieria Trifasciata",
+    common_name: "Snake Plant / Mother In-law's Tongue",
+    water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
+    light: "Thrives in medium to bright indirect light, but can tolerate low indirect light.",
+    humidity: "Average humidity levels are recommended, but these plants can tolerate dry air conditions.",
+    succulent: false,
+    pet_friendly: false,
+    bio: "Native to southern and central Africa, the Sansevieria trifasciata 'Laurentii' is a cultivar of snake plant with yellow variegated edges. Snake plants are tropical plants that are also succulents. Often positioned as low light plants, they are actually medium to bright light plants that tolerate, but not thrive in low light. Like other succulent plants, they have a modified form of photosynthesis, where in order to preserve water, the plant opens its pores exclusively at night. However, since photosynthesis only happens with daylight, the plant stores carbon dioxide acquired at night for use during the day, and releases oxygen (one of the end products of photosynthesis) at night when the pores open. This helps it to maximize efficiency in cleaning the air, as found in study conducted by NASA."
+)
+pilea3 = Plant.create(
+    picture: "https://images-na.ssl-images-amazon.com/images/I/71Mu-27B53L._SX425_.jpg",
+    binomial_name: "Pilea Peperomioides",
+    common_name: "Coin Plant / Chinese Money Plant",
+    water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
+    light: "Thrives in bright direct light, but can tolerate bright to medium indirect light.",
+    humidity: "These plants love a high himidity environment.",
+    succulent: false,
+    pet_friendly: true,
+    bio: "Pilea peperomioides (Urticaceae) is an Asiatic perennial herb that is native to Yunnan Province in Southern China at the foot of the Himalayas. For a long time, this plant has been a mystery. That is because this plant, like many other Pilea, is insignificant in economic value (weedy), and like other Pilea, not much attention was paid to it. It was found to have been brought to Norway by a Norwegian missionary in 1946, upon the expulsion of religion and foreigners by Chairman Mao. From Norway, it was distributed by friends to friends, and eventually all over the world as a houseplant."
+)
+zz3 = Plant.create(
+    picture: "https://cdn.shopify.com/s/files/1/1706/1307/products/Zamioculcas-zamiifolia-ZZ-Plant.jpg?v=1535278807",
+    binomial_name: "Zamioculcas Zamiifolia",
+    common_name: "ZZ Plant",
+    water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
+    light: "Thrives in medium to low indirect light. Can tolerate bright indirect light. Not suited for intense, direct sun.",
+    humidity: "Average indoor humidity (around 40% relative humidity) is fine for the zz plant.",
+    succulent: true,
+    pet_friendly: false,
+    bio: "Zamioculcas zamiifolia, affectionately called the ZZ plant or Zanzibar Gem, is a tropical plant in the aroid family, Araceae. It is native to Zanzibar, Kenya, and Eastern Africa. You might spot large potato-like rhizomes under the surface of its potting mix. Having evolved in drier conditions than most other aroids, these plants have evolved rhizomes that store water to help the ZZ plant survive drought in its natural environment. The rhizomes are actually underground stems, and what you see above ground are the stems' leaves. Using these underground rhizomes, the plant slowly creeps over time to new locations, spawning new aboveground leaves along the way."
+)
+snake3 = Plant.create(
+    picture: "https://images-na.ssl-images-amazon.com/images/I/51YIe2wBxlL._SX425_.jpg",
     binomial_name: "Sansevieria Trifasciata",
     common_name: "Snake Plant / Mother In-law's Tongue",
     water: "Water every 2-3 weeks, allowing soil to dry out between waterings. Increase frequency with increased light.",
