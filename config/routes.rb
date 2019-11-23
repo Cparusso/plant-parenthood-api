2901786143ed79cde3dd8c1d0b527f8eaf9a1b41
Charlie Russo <cparusso@gmail.com>
2019-11-23 16:42:59 -0500

    Small adjustment to the routes - Still have not spend much time on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :user_plants
-  resources :plants
+  resources :plants, only: [:index]
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
