531afc753a502356a92ddf2c24c57afc594e47a9
Charlie Russo <cparusso@gmail.com>
2020-01-05 14:50:13 -0500

    Adjusted the user serializer to give, more informaion about the user plants, probably too much information but for now this is fine.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,23 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :username, :email, :picture, :date_of_birth, :user_plants
+
+  def user_plants
+    self.object.user_plants.map do |user_plant|
+      {
+        id: user_plant.id,
+        picture: user_plant.picture,
+        given_name: user_plant.given_name,
+        date_received: user_plant.date_received,
+        bio: user_plant.bio,
+        plant_id: user_plant.plant_id,
+        user_id: user_plant.user_id,
+        plant: get_plant(user_plant.plant_id),
+        parent_plant_id: user_plant.parent_plant_id,
+      }
+    end
+  end
+
+  def get_plant(plant_id)
+    return Plant.find_by id: plant_id
+  end
 end
